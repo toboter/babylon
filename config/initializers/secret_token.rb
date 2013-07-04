b656be5090acbf9bd1dc33444348f392c0b3d0d4
Tobias Schmidt <t.schmidt@rubidat.de>
2013-07-04 11:37:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babylon::Application.config.secret_token = '2845fdf3e31905ef56aa55d09778be67f69d4f6709cbe85770e601e4d40dd175d613753ea4f72e37ff64185ea2569a32d747e2329a75fae9e4363c527041b590'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Babylon::Application.config.session_store :cookie_store, key: '_babylon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Babylon::Application.config.session_store :active_record_store
