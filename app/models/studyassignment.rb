461a4a4410cb6a1e6e26a46a4af6617d593bf9f0
Tobias Schmidt <t.schmidt@rubidat.de>
2014-08-29 12:52:37 +0200

    0.1.0.5

--- app/models/studyassignment.rb
+++
@@ -1,6 +0,0 @@
-class Studyassignment < ActiveRecord::Base
-  attr_accessible :item_id, :project_id, :creator_id, :updater_id
-
-  belongs_to :item
-  belongs_to :project
-end
--- db/migrate/20140731131037_add_sort_order_to_item_classifications.rb
+++
@@ -1,5 +0,0 @@
-class AddSortOrderToItemClassifications < ActiveRecord::Migration
-  def change
-    add_column :item_classifications, :sort_order, :integer
-  end
-end
